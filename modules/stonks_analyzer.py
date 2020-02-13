bef2607f3c990e5966f47d62d0ebd230aa8b0c79
Kennedy <matthew.kennedy1@stryker.com>
2020-02-13 08:15:15 -0500

    Finished analyzer primitive version

--- modules/stonks_analyzer.py
+++ modules/stonks_analyzer.py
@@ -35,10 +35,10 @@ class stonks_analyzer:
         self.cash = float([line.rstrip('\n') for line in open(self.cash_path, 'r')][0])
         
         #Variables for trend/growth
-        self.growth_range = 20
+        self.growth_range = 1 #Go 1 back
         self.shrink_percent = 0
         self.growth_percent = 0.2
-        self.growth_amount = 0.5
+        self.growth_amount = 1
 
     #Check each company stock prices, see if any worth buying/selling
     def check_stocks(self, messageQ=None):
@@ -52,52 +52,64 @@ class stonks_analyzer:
                 file.write(line + "\n")                    
             file.truncate()
 
+        """
         with open(self.cash_path, "r+") as file:
             file.seek(0)
             file.write(str(self.cash))                    
             file.truncate()
-            
+        """
+
+    """
+    Algorithm for selling:
+    Loop through each company in current stocks file.
+    Get the index of the company, get the price info on the company for the last 2
+    Check the # of times decreasing: if 3, then sell.
+    If decreasing but hasn't happened 3 times, increment.
+    If increasing, decrement count by 1 until 0.
+    """
     def sell_stocks(self, messageQ):
-        #For each stock, get the company, find the data in data_contents, see the trend, act
+        #If there are no current stocks, just return.
         if len(self.current_stocks) == 0:
             return
+        
         cashMade = 0
-        for company in range(0, len(self.current_stocks[0]), 2):
-            """
-            for row in self.current_stocks[1:]:
-                
-                
-                #This is for using the buy point as reference
-                buyDate = row[company + 1]
-                try:
-                    buyDate  = datetime.datetime.strptime(row[company+1], "%m/%d/%Y %H:%M:%S").date()
-                except ValueError:
-                    buyDate  = datetime.datetime.strptime(row[company+1], "%m/%d/%Y %H:%M").date()
-                #Get the last recorded price value
-                priceInfo = self.stonks_sorter.get_price_diff(company, buyDate)
-                """
-            
-            #Decide to stay or sell
+        #For each stock, get the company, find the data in data_contents, see the trend, act
+        for company in range(0, len(self.current_stocks[0]), 2):            
             company_index = self.data_contents[0].index(self.current_stocks[0][company])
             priceInfo = self.stonks_sorter.get_price_diff(company_index, index=self.growth_range)
-            if priceInfo[3] < self.shrink_percent:
-                for stock in range(1, len(self.current_stocks)):
-                    if len(self.current_stocks[stock]) <= company:
-                        break
-                    #Get last row for that one
-                    moneyMade = self.get_last_price(company) - float(self.current_stocks[stock][company])
-                    self.cash += moneyMade
-                    cashMade+= moneyMade
-                    self.current_stocks[stock][company] = ''
-                    self.current_stocks[stock][company+1] = ''
+
+            #Loop through each row with row var as stock
+            for stock in range(1, len(self.current_stocks)):
+                #Check if there are any stocks in the row for the company. If not, break
+                if len(self.current_stocks[stock]) <= company:
+                    break
+
+                #See how many times var has shrinked
+                shrinkCount = self.current_stocks[stock][company+1]
+                if shrinkCount:
+                    shrinkCount = int(shrinkCount)
+
+                    #If it's less than 3, increment. If 3, sell.
+                    if shrinkCount < 3:
+                        shrinkCount+=1
+                        self.current_stocks[stock][company+1] = str(shrinkCount)
+                    else:
+                        moneyMade = self.get_last_price(company) - float(self.current_stocks[stock][company])
+                        self.cash += moneyMade
+                        cashMade+= moneyMade
+                        self.current_stocks[stock][company] = ''
+                        self.current_stocks[stock][company+1] = ''
 
         if messageQ:
             messageQ.put(cashMade)
+
+    
     def buy_stocks(self, messageQ):
         #Get increase over last range, buy if increasing
         cashSpent = 0
         for company in range(0, len(self.data_contents[0]), 2):
             priceInfo = self.stonks_sorter.get_price_diff(company, index=self.growth_range)
+            
             if priceInfo[3] > self.growth_percent or priceInfo[2] > self.growth_amount:
                 price = self.get_last_price(company)
                 if self.cash < price:
@@ -105,19 +117,35 @@ class stonks_analyzer:
                 
                 self.cash -= price
                 cashSpent += price
-                last_row = self.get_last_row(company)
-                if last_row == -2:
+                company_index = -1
+                if len(self.current_stocks) != 0:
+                    try:
+                        company_index = self.current_stocks[0].index(self.data_contents[0][company])
+                    except ValueError:
+                        pass #means company not in list, index still -1
+                first_row = self.get_first_row(company_index)
+
+                if first_row == -2: #No current stocks at all
                     self.current_stocks.append([self.data_contents[0][company]])
                     self.current_stocks[0].append('')
-                    self.current_stocks.append([str(price), datetime.datetime.now().strftime("%m/%d/%Y %H:%M:%S")])
-                elif last_row == -1:
+                    self.current_stocks.append([str(price), "0"])
+                elif first_row == -1: #Company not in the stock spreadsheet
                     self.current_stocks[0].append(self.data_contents[0][company])
                     self.current_stocks[0].append('')
-                    self.current_stocks[last_row].append(str(price))
-                    self.current_stocks[last_row].append(datetime.datetime.now().strftime("%m/%d/%Y %H:%M:%S"))
+                    self.current_stocks[first_row].append(str(price))
+                    self.current_stocks[first_row].append("0")
                 else:
-                    self.current_stocks[last_row][company] = str(price)
-                    self.current_stocks[last_row][company+1] = datetime.datetime.now().strftime("%m/%d/%Y %H:%M:%S")
+                    if first_row == len(self.current_stocks):
+                        
+                        self.current_stocks.append(['']*len(self.current_stocks[0]))
+                        self.current_stocks[first_row][company_index] = str(price)
+                        self.current_stocks[first_row][company_index+1] = "0"
+                    else:
+                        #print("here")
+                        #print(last_row)
+                        self.current_stocks[first_row][company_index] = str(price)
+                        self.current_stocks[first_row][company_index+1] = "0"
+        #print(self.current_stocks)           
         if messageQ:
             messageQ.put(cashSpent)
 
@@ -136,23 +164,31 @@ class stonks_analyzer:
         #Get the prices starting at the last recorded price value
         return float(self.data_contents[last_row][company])
 
-    def get_last_row(self, company):
+    #Get first open row
+    def get_first_row(self, company_index):
         if len(self.current_stocks) == 0:
             return -2
-        #Get the last recorded price value
-        if not company in self.current_stocks[0]:
+
+        #company not in list
+        if company_index == -1:
             return -1
-        last_row = len(self.current_stocks) - 1
-        empty_row = True
+
+        first_row = 1
+        filled_row = True
+
         
-        while empty_row:
-            if not self.data_contents[last_row][company]:
-                last_row-=1
+        #Work forwards starting at column
+        while filled_row:
+            if first_row == len(self.current_stocks):
+                break
+            elif self.current_stocks[first_row][company_index]:
+                first_row+=1
                 continue
-            empty_row = False
+            
+            filled_row = False
 
         #Get the prices starting at the last recorded price value
-        return last_row
+        return first_row
 
     
 #Structure:     
