c6861133b44207c18acac792a1c9fad7b39dd920
Kennedy <matthew.kennedy1@stryker.com>
2020-02-14 10:59:10 -0500

    Changed buying model and growth range

--- modules/stonks_analyzer.py
+++ modules/stonks_analyzer.py
@@ -36,16 +36,20 @@ class stonks_analyzer:
         self.stonks_sorter = stonks_sorter(self.data_contents)
         self.companies = [line.rstrip('\n') for line in open(self.company_links_path, 'r')]
         self.cash = float([line.rstrip('\n') for line in open(self.cash_path, 'r')][0])
+        self.cash_spent = 3000
         
         #Variables for trend/growth
         self.shrink_range = 1 #Go 1 back
         self.shrink_percent = 0
-        self.growth_range = 1 
+        self.growth_range = 5
         self.growth_percent = 0.2
-        self.growth_amount = 1
+        self.growth_amount = 0.2
 
     #Check each company stock prices, see if any worth buying/selling
     def check_stocks(self, messageQ=None):
+        self.cash += 100
+        self.cash_spent += 100
+        
         self.sell_stocks(messageQ)
         self.buy_stocks(messageQ)
         
@@ -104,51 +108,73 @@ class stonks_analyzer:
                         else:
                             company_index = self.data_contents[0].index(self.current_stocks[0][company])
                             self.cash += self.get_last_price(company_index)
-                            print("Sold " + self.current_stocks[0][company] + " for " + str(moneyMade))
+                            print("Sold " + self.current_stocks[0][company] + " for " + str(self.get_last_price(company_index)))
                             self.current_stocks[stock][company] = ''
                             self.current_stocks[stock][company+1] = ''
 
-    
+    """
+    Buy stocks
+    Currently checks growth over last 5 prices, see if growing, buy
+    """
     def buy_stocks(self, messageQ):
         #Get increase list
+        """
         self.increase_list = self.stonks_sorter.get_increase_list(
                                         500,
                                         self.stonks_sorter.get_earliest_date("Day"),
                                         "Increase",
                                         "$")
-        for company in range(len(self.increase_list)-1, 0, -1):
-            #Growth is too minimal to invest
-            if self.increase_list[company][2] < self.growth_percent and self.increase_list[company][1] < self.growth_amount:
-                return
+        """
+        #Get a list of highest growth rates (% and $) over last x indeces
+        self.increase_list = []
+        for company in range(0, len(self.data_contents[0]), 2):
+            priceInfo = self.stonks_sorter.get_price_diff(company, index=self.growth_range)
+            self.increase_list.append([self.data_contents[0][company], priceInfo[0], priceInfo[1], priceInfo[2], priceInfo[3]])
             
-            data_contents_index = self.data_contents[0].index(self.increase_list[company][0])
+        self.increase_cash = sorted(self.increase_list, key = lambda x: x[3])
+        self.increase_percent = sorted(self.increase_list, key = lambda x: x[4])
+        self.buy_list = []
+        
+        #price diff is 3, % diff is 4
+        for company in range(len(self.increase_cash)-1, 0, -1):
+            should_buy = self.check_to_buy(company, "$")
+            if should_buy == -1:
+                break
+            elif should_buy == 1:
+                self.buy_list.append(self.increase_cash[company])
+
+        for company in range(len(self.increase_percent)-1, 0, -1):
+            should_buy = self.check_to_buy(company, "%")
+            if should_buy == -1:
+                break
+            elif should_buy == 1:
+                self.buy_list.append(self.increase_cash[company])
+
+        for company in self.buy_list:
+            
+            data_contents_index = self.data_contents[0].index(company[0])
             price = self.get_last_price(data_contents_index)
-            
-            if self.cash < price:
-                continue
-            
-            self.cash -= price
             company_index = -1
+            
             if len(self.current_stocks) != 0:
                 try:
-                    company_index = self.current_stocks[0].index(self.increase_list[company][0])
+                    company_index = self.current_stocks[0].index(company[0])
                 except ValueError:
                     pass #means company not in list, index still -1
                 
             first_row = self.get_first_row(company_index)
 
             if first_row == -2: #No current stocks at all
-                self.current_stocks.append([self.increase_list[company][0]])
+                self.current_stocks.append([company[0]])
                 self.current_stocks[0].append('')
                 self.current_stocks.append([str(price), "0"])
             elif first_row == -1: #Company not in the stock spreadsheet
-                self.current_stocks[0].append(self.increase_list[company][0])
+                self.current_stocks[0].append(company[0])
                 self.current_stocks[0].append('')
                 self.current_stocks[first_row].append(str(price))
                 self.current_stocks[first_row].append("0")
             else:
                 if first_row == len(self.current_stocks):
-                    print(self.current_stocks, company_index)
                     self.current_stocks.append(['']*len(self.current_stocks[0]))
                     self.current_stocks[first_row][company_index] = str(price)
                     self.current_stocks[first_row][company_index+1] = "0"
@@ -157,6 +183,9 @@ class stonks_analyzer:
                     self.current_stocks[first_row][company_index+1] = "0"
 
 
+    """
+    Get total value of stocks/cash
+    """
     def get_total_value(self, messageQ):
         total_value = self.cash
         for row in self.current_stocks[1:]:
@@ -165,10 +194,44 @@ class stonks_analyzer:
                     total_value += float(value)
         
         if not messageQ:
-            return total_value
+            return total_value - self.cash_spent
         else:
-            messageQ.put(total_value)
-            
+            messageQ.put(total_value - self.cash_spent)
+
+    """
+    Helper function
+    See if should buy stock
+    """
+    def check_to_buy(self, company, variable):
+        #Growth is too minimal to invest
+        if variable == "$":
+            if self.increase_cash[company][3] < self.growth_amount:
+                return -1
+        elif variable == "%":
+            if self.increase_cash[company][4] < self.growth_percent:
+                return -1
+
+        #Get the index in data contents
+        if variable == "$":
+            data_contents_index = self.data_contents[0].index(self.increase_cash[company][0])
+        elif variable == "%":
+            data_contents_index = self.data_contents[0].index(self.increase_percent[company][0])
+
+        #See if have the money to buy
+        price = self.get_last_price(data_contents_index)
+        if self.cash < price:
+            return 0
+
+        #If company not already in list, buy
+        if not any(self.increase_cash[company][0] in company_info for company_info in self.buy_list):
+            self.cash -= price
+            return 1
+        return 0
+
+    """
+    Helper function
+    Get last price recorded in self.data_contents
+    """
     def get_last_price(self, company):
         #Get the last recorded price value
         last_row = len(self.data_contents) - 1
@@ -183,7 +246,10 @@ class stonks_analyzer:
         #Get the prices starting at the last recorded price value
         return float(self.data_contents[last_row][company])
 
-    #Get first open row
+    """
+    Helper function
+    Get the first open row in the self.current_stocks spreadsheet
+    """
     def get_first_row(self, company_index):
         try:
             if len(self.current_stocks) == 0:
@@ -195,7 +261,6 @@ class stonks_analyzer:
 
             first_row = 1
             filled_row = True
-
         
             #Work forwards starting at column
             while filled_row:
@@ -232,4 +297,4 @@ class stonks_analyzer:
 #Check growth since buy price. If growth < 0 n times consecutively or losing > 1%, sell
 if __name__ == '__main__':
     analyzer = stonks_analyzer()
-    analyzer.check_stocks()
+    print(analyzer.check_stocks())
--- modules/test.py
+++ modules/test.py
@@ -1,3 +1,19 @@
-test = [1,2,3,4,5,6,7,8]
-for i in range(0, len(test), -1):
-    print(i)
+import requests
+from bs4 import BeautifulSoup
+url='http://eoddata.com/stocklist/NYSE/A.htm'
+page = requests.get(url)
+
+soup = BeautifulSoup(page.text, 'html.parser')
+table = soup.find("table", {"class": "quotes"})
+stocks = table.findAll("tr")
+symbols = []
+
+token_start = 'onclick="location.href=' + "'/stockquote/NYSE/"
+token_end = ".htm'"
+#print(stocks[1])
+for stock in stocks[1:]:
+    str_stock = str(stock)
+    symbols.append(str_stock[str_stock.find(token_start) + len(token_start):
+                             str_stock.find(token_end)])
+for symbol in symbols:
+    print(symbol)
